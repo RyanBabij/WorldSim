3c72c4753171194c48aae1bbb1fd4b2eef509b59
Ryan Babij <s3589028@student.rmit.edu.au>
2018-07-16 17:53:36 +1000

    Version string update

--- Driver_Settings.cpp
+++ Driver_Settings.cpp
@@ -19,7 +19,7 @@ int lastline = 0;
 
 bool FAST_COUT = false;
 
-const std::string VERSION = "0.0.011 Win32 dev";
+const std::string VERSION = "0.0.017 Win32 dev";
 const std::string G_WINDOW_TITLE = "WorldSim";
 
 const std::string SAVE_FOLDER_PATH = "savedata";
